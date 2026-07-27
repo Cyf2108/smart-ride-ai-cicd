a18db7da6c9a000be1e22985efc9c515eb713e29
Chin Yung Fei <yungfeichin@gmail.com>
2026-07-27 15:50:45 +0800

    Test

--- app/train_model.py
+++ app/train_model.py
@@ -3,9 +3,9 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
 data = {
-    "distance_km":   [2, 5, 10, 3, 8, 15, 20, 7, 12, 1],
-    "traffic_level": [1, 2, 3, 1, 2, 3, 3, 2, 1, 3],
-    "fare":          [6, 10, 20, 7, 18, 40, 45, 12, 22, 5]
+    "distance_km":   [2, 5, 10, 3, 8, 15, 20, 7, 12, 1, 5],
+    "traffic_level": [1, 2, 3, 1, 2, 3, 3, 2, 1, 3, 5],
+    "fare":          [6, 7, 20, 7, 18, 40, 45, 12, 22, 5, 10]
 }
 
 df = pd.DataFrame(data)
